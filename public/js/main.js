33daa569ee3e9aa64088834ebb88db5f6ce2eac9
Kiran Wattamwar <kiranwattamwar@Kirans-MacBook-Pro.local>
2019-07-03 09:49:14 -0400

    More groundwork

--- public/js/main.js
+++ public/js/main.js
@@ -46,8 +46,32 @@ $(document).ready(function(){
     progressBar.setCurrentStep(0);
 
 
+    //Matrix accordion (vertical)
+    var acc = document.getElementsByClassName("matrix-accordion");
+    var i;
 
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            /* Toggle between adding and removing the "active" class,
+            to highlight the button that controls the panel */
+            this.classList.toggle("matrix-active");
 
+            /* Toggle between hiding and showing the active panel */
+            var panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+            if (panel.style.maxHeight){
+                panel.style.maxHeight = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            }
+        });
+    }
+
+    // Matrix accordion (horizontal)
     particlesJS("particles-js", {
         "particles": {
             "number": {
